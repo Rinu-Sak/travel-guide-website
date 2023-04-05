f78b0f2f6efc4156b44060a6731cd07210a669c3
Rinu Sakeer <renarinu7823@gmail.com>
2023-04-05 21:57:44 +0530

    created footer section

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const menuBtn = document.querySelector('.menu-btn')
 const navLinks = document.querySelector('.nav-links')
 
-
+ 
 menuBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('.mobile-menu');
+    navLinks.classList.toggle('.mobile-menu') 
 
 })
+// navLinks.style.right = "-900px";
+// menuBtn.onclick = function(){
+//     if(navLinks.style.right == "-900px"){
+//         navLinks.style.right = "0";
+//         menu.src = "close.png";
+
+//     }
+//     else{
+//         navLinks.style.right = "-900px";
+//         menu.src = "menubar.png";
+
+//     }
+// }
